4a67ca715caedfab26563312f6f25f516fb009e3
thien2001git <hoangthien66771508@gmail.com>
2024-10-26 14:42:56 +0700

    🎇 [BE] fix 404 err

--- api/index.ts
+++ api/index.ts
@@ -11,7 +11,7 @@ const cors = require('cors');
 app.use(
     cors(
         {
-            origin: ['my front end http'],
+            origin: ['*'],
             methods: ['GET', 'POST', 'PUT', 'DELETE'],
             allowedHeaders: ['Content-Type', 'Authorization'],
             credentials: true
