ce860beabe0f0c961cbdec738a944026c86aaba0
thien2001git <hoangthien66771508@gmail.com>
2024-10-26 15:30:30 +0700

    🎇 [BE] use all cors

--- api/index.ts
+++ api/index.ts
@@ -8,16 +8,7 @@ const cookieParser = require('cookie-parser')
 const logger = require('morgan')
 const cors = require('cors');
 
-app.use(
-    cors(
-        {
-            origin: ['*'],
-            methods: ['GET', 'POST', 'PUT', 'DELETE'],
-            allowedHeaders: ['Content-Type', 'Authorization'],
-            credentials: true
-        }
-    )
-)
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
