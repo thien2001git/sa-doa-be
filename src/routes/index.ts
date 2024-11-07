84a1a76639a97ff6cdb7f838845b219c89b2535d
Quang Trong <quangtrong1506@gmail.com>
2024-11-07 19:48:16 +0700

    fix(Routes): :bug: fix apply `Middleware` anywhere

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,11 @@
 import express from 'express';
 import { responseErrors, responseSuccess } from '../helpers';
 import authRouter from './auth';
-import useRouter from './users';
+import userRouter from './users';
 const router = (app: express.Express) => {
     app.get('/', (_, res) => responseSuccess(res, null, 200, 'Thành công'));
     authRouter(app);
-    useRouter(app);
+    userRouter(app);
     app.all('*', (_, res) => responseErrors(res, 'API Not Found', 404));
 };
 export default router;
--- src/routes/users/index.ts
+++ src/routes/users/index.ts
@@ -2,10 +2,10 @@ import express from 'express';
 import UserController from '../../api/src/presentation/Controllers/UserController';
 import { authMiddleware } from '../../api/src/presentation/middlewares/auth.middleware';
 
-const useRouter = (app: express.Express) => {
+const userRouter = (app: express.Express) => {
     const router = express.Router();
     // domain/user/1
-    app.use(authMiddleware);
+    router.use(authMiddleware);
     router.get('/', UserController.index);
     router.post('/create', UserController.create);
     router.put('/update', UserController.update);
@@ -13,4 +13,4 @@ const useRouter = (app: express.Express) => {
     app.use('/users', router); // domain/user
 };
 
-export default useRouter;
+export default userRouter;
